7c10cff5596bab8e7b3ab2a37601d2d3653fbdd7
david_vl2003@hotmail.com <david_vl2003@hotmail.com>
2025-04-09 18:53:49 -0600

    Addition of URDF and gazebo spawner

---
+++ packages/puzzlebot_description/scripts/puzzlebot_gazebo_spawner.launch.py
@@ -0,0 +1,75 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, FindExecutable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+    pkg_name = "puzzlebot_description"
+
+    # Paths
+    pkg_share = FindPackageShare(pkg_name).find(pkg_name)
+    default_model_path = os.path.join(pkg_share, "urdf", "puzzlebot.urdf")
+    default_rviz_config_path = os.path.join(pkg_share, "rviz", "visualizer.rviz")
+
+    return LaunchDescription([
+        # Launch arguments
+        DeclareLaunchArgument(
+            name="model", default_value=default_model_path,
+            description="Absolute path to robot urdf.xacro file"
+        ),
+        DeclareLaunchArgument(
+            name="rviz", default_value="false",
+            description="Launch RViz?"
+        ),
+
+        # Launch Gazebo
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare("gazebo_ros"),
+                    "launch", "gazebo.launch.py"
+                ])
+            ])
+        ),
+
+        # State publisher
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[{
+                "robot_description": Command([
+                    FindExecutable(name="xacro"),
+                    " ",
+                    LaunchConfiguration("model")
+                ])
+            }],
+            output="screen"
+        ),
+
+        # Spawn robot in Gazebo
+        Node(
+            package="gazebo_ros",
+            executable="spawn_entity.py",
+            arguments=[
+                "-topic", "robot_description",
+                "-entity", "lidar_cube"
+            ],
+            output="screen"
+        ),
+
+        # Optional RViz launch
+        Node(
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="screen",
+            arguments=["-d", default_rviz_config_path]
+        )
+    ])
