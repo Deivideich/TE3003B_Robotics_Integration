95ae184d1996b74f25eaaf9b873ac803ffb192d7
david_vl2003@hotmail.com <david_vl2003@hotmail.com>
2025-04-10 15:25:06 -0600

    Wheel transforms are now visible in rviz

--- packages/puzzlebot_description/scripts/puzzlebot_gazebo_spawner.launch.py
+++ packages/puzzlebot_description/scripts/puzzlebot_gazebo_spawner.launch.py
@@ -39,14 +39,7 @@ def generate_launch_description():
                 ])
             ])
         ),
-        
-        Node(
-            package="joint_state_publisher",
-            executable="joint_state_publisher",
-            name="joint_state_publisher",
-            output="screen"
-        ),
-
+    
         # State publisher
         Node(
             package="robot_state_publisher",
