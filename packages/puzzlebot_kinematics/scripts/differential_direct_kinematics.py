aeb3f2ff93f6d45892072b004030bebdc75aac0b
JuanCarlosHR2003 <jchr.2003.mx@gmail.com>
2025-05-09 13:47:59 -0600

    added covariance in odom

--- packages/puzzlebot_kinematics/scripts/differential_direct_kinematics.py
+++ packages/puzzlebot_kinematics/scripts/differential_direct_kinematics.py
@@ -7,6 +7,7 @@ from nav_msgs.msg import Odometry
 from tf2_ros import TransformBroadcaster, TransformStamped
 import math
 import time
+import numpy as np
 
 class DeadReckoning(Node):
     def __init__(self):
@@ -20,6 +21,16 @@ class DeadReckoning(Node):
         self.x = 0.0
         self.y = 0.0
         self.theta = 0.0
+
+        # Covariance parameters
+        self.kr = 0.5  # covariance for right wheel
+        self.kl = 0.5  # covariance for left wheel
+
+        # Covariance matrix
+        self.cov = np.zeros((3, 3))
+
+        #Last theta
+        self.last_theta = 0.0
 
         # Time tracking
         self.last_time = self.get_clock().now().seconds_nanoseconds()[0] + \
@@ -37,6 +48,54 @@ class DeadReckoning(Node):
         self.odom_pub = self.create_publisher(Odometry, '/odom', 10)
         self.tf_broadcaster = TransformBroadcaster(self)
 
+    def model_jacobian(self, v, dt, theta):
+        H_k = np.array([[1, 0, -dt * v * np.sin(theta)],
+                         [0, 1, dt * v * np.cos(theta)],
+                         [0, 0, 1]])
+        
+        return H_k
+    
+    def error_matrix(self, r, dt, theta, l, omega_l, omega_r):
+        delta_wk = 0.5 * r * dt * np.array([[np.cos(theta), np.cos(theta)],
+                                             [np.sin(theta), np.sin(theta)],
+                                             [2 / l, -2 / l]])
+        sigma_delta_k = np.array([[self.kr * omega_r, 0],
+                                   [0, self.kl * omega_l]])
+        
+        Q_k = delta_wk @ sigma_delta_k @ delta_wk.T
+        return Q_k
+    
+    def model_covariance(self, v, dt, theta, last_cov, omega_l, omega_r):
+        # Jacobian of the motion model
+        H_k = self.model_jacobian(v, dt, theta)
+
+        # Process noise covariance
+        Q = self.error_matrix(self.wheel_radius, dt, theta, self.wheel_base, omega_l, omega_r)
+        # Q = np.array([[0.5, 0.01, 0.01],
+        #               [0.01, 0.5, 0.01],
+        #               [0.01, 0.01, 0.2]])
+
+        # Compute the covariance of the state
+        cov = H_k @ last_cov @ H_k.T + Q
+        
+        self.get_logger().info(f'Covariance: {cov}')
+
+        return cov
+    
+    def convert_covariance_puzzlebot_to_ros(self, cov):
+        # Convert covariance matrix to ROS format
+        ros_cov = np.zeros(36)
+        ros_cov[0] = cov[0, 0]
+        ros_cov[1] = cov[0, 1]
+        ros_cov[5] = cov[0, 2]
+        ros_cov[6] = cov[1, 0]
+        ros_cov[7] = cov[1, 1]
+        ros_cov[11] = cov[1, 2]
+        ros_cov[30] = cov[2, 0]
+        ros_cov[31] = cov[2, 1]
+        ros_cov[35] = cov[2, 2]
+
+        return ros_cov
 
     def wheel_callback(self, msg):
         current_time = self.get_clock().now().seconds_nanoseconds()
@@ -54,6 +113,8 @@ class DeadReckoning(Node):
         # Direct kinematics
         v = self.wheel_radius * (omega_r + omega_l) / 2
         w = self.wheel_radius * (omega_r - omega_l) / self.wheel_base
+
+
 
         # Update pose
         self.x += v * math.cos(self.theta) * dt
@@ -78,7 +139,9 @@ class DeadReckoning(Node):
         odom.pose.pose.orientation.w = qw
 
         # Zero covariance
-        odom.pose.covariance = [0.0] * 36
+        self.cov = self.model_covariance(v, dt, self.last_theta, self.cov, omega_l, omega_r)
+        ros_cov = self.convert_covariance_puzzlebot_to_ros(self.cov)
+        odom.pose.covariance = ros_cov
         odom.twist.covariance = [0.0] * 36
 
         odom.twist.twist.linear.x = v
@@ -98,6 +161,7 @@ class DeadReckoning(Node):
         t.transform.rotation.w = qw
 
         self.tf_broadcaster.sendTransform(t)
+        self.last_theta = self.theta
         # self.get_logger().info(f'Pose: x={self.x:.2f}, y={self.y:.2f}, theta={self.theta:.2f}')
 
 
